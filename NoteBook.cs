d13e10f0427b12d9ea89e49d9003a4215b3294bd
Nauryzbai <nauzik@mail.ru>
2019-03-26 17:19:25 +0600

    XML и JSON классы

---
+++ NoteBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class NoteBook
+    {
+        public string Model { get; set; }
+        public int Mark { get; set; }
+        public string Memory { get; set; }
+        public string OC { get; set; }
+        public string Processor { get; set; }
+        public int Display { get; set; }
+    }
+}
